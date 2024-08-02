a8d9d6e8dfd6a2d4b8a98c3234e19d6895db62f2
Weiqiang Liu <96517728+solutide1211@users.noreply.github.com>
2024-08-03 06:39:14 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "So_Blag"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
+author = "So"
 email = "hi@imalan.cn"
 author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+description = "学习使我快乐。"
+key_words = ['Maverick',  'Galileo', 'blog']
 language = 'zh-CN'
 external_links = [
     {
